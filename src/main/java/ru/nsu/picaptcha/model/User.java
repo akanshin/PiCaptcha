a292421ea35b053e970d5e32b32b8eabdd35a416
a.fink <a.fink@eastbanctech.ru>
2018-12-19 20:36:22 +0700

    fix user

--- src/main/java/ru/nsu/picaptcha/model/User.java
+++ src/main/java/ru/nsu/picaptcha/model/User.java
@@ -4,8 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
-import org.springframework.data.annotation.Id;
-import javax.persistence.*;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -16,25 +23,25 @@ import java.util.Set;
 @Table(name = "users")
 public class User {
 
-  @Id
-  @GeneratedValue(generator = "increment")
-  @GenericGenerator(name = "increment", strategy = "increment")
-  private long id;
+    @Id
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(name = "increment", strategy = "increment")
+    private long id;
 
-  @Column(name = "login", nullable = false)
-  private String login;
+    @Column(name = "login", nullable = false)
+    private String login;
 
-  @Column(name = "is_banned", nullable = false)
-  private Boolean isBanned = false;
+    @Column(name = "is_banned", nullable = false)
+    private Boolean isBanned = false;
 
-  @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-  private Set<User> friends = new HashSet<>();
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<User> friends = new HashSet<>();
 
-  public boolean addFriend(User user) {
-    return friends.add(user);
-  }
+    public boolean addFriend(User user) {
+        return friends.add(user);
+    }
 
-  public boolean removeFriend(User user) {
-    return friends.remove(user);
-  }
+    public boolean removeFriend(User user) {
+        return friends.remove(user);
+    }
 }
