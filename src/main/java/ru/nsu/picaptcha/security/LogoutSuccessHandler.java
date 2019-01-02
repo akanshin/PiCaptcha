e25e89ce6d900cae860abf0239a32bea20c7f7ce
VictoriaKostyleva <kostyleva.viktoriya@gmail.com>
2019-01-02 20:51:44 +0700

    changed inmemory db to postgres

--- src/main/java/ru/nsu/picaptcha/security/LogoutSuccessHandler.java
+++ src/main/java/ru/nsu/picaptcha/security/LogoutSuccessHandler.java
@@ -4,17 +4,20 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
 public class LogoutSuccessHandler extends SimpleUrlLogoutSuccessHandler {
+
     @Override
     public void onLogoutSuccess(
             HttpServletRequest request, HttpServletResponse response, Authentication authentication
-    ) throws IOException {
+    ) throws IOException, ServletException {
         response.setContentType("text/html;charset=UTF-8");
         response.getWriter().println("LogoutSuccessful");
+        super.onLogoutSuccess(request, response, authentication);
     }
 }
