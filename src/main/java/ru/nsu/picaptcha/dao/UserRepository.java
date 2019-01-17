aec529ddfb1b3b861685c358d8aa7344d279c1cf
a.fink <a.fink@eastbanctech.ru>
2019-01-17 20:30:30 +0700

    FIX

--- src/main/java/ru/nsu/picaptcha/dao/UserRepository.java
+++ src/main/java/ru/nsu/picaptcha/dao/UserRepository.java
@@ -5,4 +5,6 @@ import ru.nsu.picaptcha.model.User;
 
 public interface UserRepository extends CrudRepository<User,Long> {
 
+    User findByLogin(String login);
+
 }
--- src/main/java/ru/nsu/picaptcha/security/WebSecurityConfiguration.java
+++ src/main/java/ru/nsu/picaptcha/security/WebSecurityConfiguration.java
@@ -2,40 +2,26 @@ package ru.nsu.picaptcha.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import ru.nsu.picaptcha.dao.UserRepository;
 
-import javax.sql.DataSource;
+import java.util.Collections;
 
 @Configuration
-public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter implements UserDetailsService {
 
-    private static final AuthenticationFailureHandler FAILURE_HANDLER = new SimpleUrlAuthenticationFailureHandler();
-
-    private final RestAuthenticationEntryPoint restAuthenticationEntryPoint;
     private final LoginSuccessHandler loginSuccessHandler;
-    private final LogoutSuccessHandler logoutSuccessHandler;
-    private final DataSource dataSource;
+    private final UserRepository userRepository;
 
     @Autowired
-    public WebSecurityConfiguration(RestAuthenticationEntryPoint restAuthenticationEntryPoint,
-                                    LoginSuccessHandler loginSuccessHandler,
-                                    LogoutSuccessHandler logoutSuccessHandler,
-                                    DataSource dataSource) {
-        this.restAuthenticationEntryPoint = restAuthenticationEntryPoint;
+    public WebSecurityConfiguration(LoginSuccessHandler loginSuccessHandler, UserRepository userRepository) {
         this.loginSuccessHandler = loginSuccessHandler;
-        this.logoutSuccessHandler = logoutSuccessHandler;
-        this.dataSource = dataSource;
-    }
-
-    @Autowired
-    public void configure(final AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication().dataSource(dataSource)
-                .usersByUsernameQuery("select login, password, enabled from users where login=?")
-                .authoritiesByUsernameQuery("select login, role from user_roles where login=?");
+        this.userRepository = userRepository;
     }
 
     @Override
@@ -43,26 +29,22 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         http
                 .csrf()
                 .disable()
-                .exceptionHandling()
-                .authenticationEntryPoint(restAuthenticationEntryPoint)
-                .and()
                 .authorizeRequests()
-                .antMatchers("/")
-                .permitAll()
-                .and()
-                .authorizeRequests()
-                .antMatchers("/info/special")
-                .hasRole("USER")
-                .and()
-                .authorizeRequests()
-                .antMatchers("/info/secret")
-                .hasRole("ADMIN")
+                .anyRequest().authenticated()
                 .and()
                 .formLogin()
+                .loginPage("/login")
+                .permitAll()
                 .successHandler(loginSuccessHandler)
-                .failureHandler(FAILURE_HANDLER)
                 .and()
                 .logout()
-                .logoutSuccessHandler(logoutSuccessHandler);
+                .permitAll();
+
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        ru.nsu.picaptcha.model.User byLogin = userRepository.findByLogin(s);
+        return new User(byLogin.getLogin(), byLogin.getPassword(), Collections.emptyList());
     }
 }
